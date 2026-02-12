70a39ff28c1369ade6b1194f06198c03cfb79df1
sara alsayyah <saraalsayyah2111978@gmail.com>
2026-02-12 20:20:33 +0200

    Structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-indigo-400">
+        Tailwind is working ✨
+      </h1>
+    </div>
+  );
+}
